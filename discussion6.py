623166ba66877d4d3c5840612f523f70235ad079
spx <spx@LucySdeMacBook-Air-2.local>
2026-02-18 23:35:26 -0500

    Task 4

--- discussion6.py
+++ discussion6.py
@@ -139,7 +139,19 @@ class HorseRaces:
             A dictionary with each horse and their average time.
             EXAMPLE: {'Gold Ship': 16.5, 'Daiwa Scarlet': 17.2}
         '''
-        pass
+        avg_dict = {}
+
+        for horse, races in self.race_dict.items():
+            total = 0
+            count = 0
+
+            for time in races.values():
+                total += time
+                count += 1
+
+            avg_dict[horse] = total / count
+
+        return avg_dict
 
 ###############################################################################
 ##### DO NOT MODIFY THE UNIT TESTS BELOW!
